54611996abe5b6036c19566f9c590816fe6a12d3
Pjus <wnstjd117@naver.com>
2020-07-28 13:16:51 +0900

    1st commit

---
+++ src/01.dart_crwaling.py
@@ -0,0 +1,6 @@
+# 35d444d5a2ca8c271de34c8d296baf7474c37035 my API key
+# pip install dart-fss
+import dart_fss as dart
+
+api_key='35d444d5a2ca8c271de34c8d296baf7474c37035' 
+dart.set_api_key(api_key=api_key)
